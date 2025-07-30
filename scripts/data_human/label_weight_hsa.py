e4cf81b1ad05e83e204f40bb88cd28f65e266d12
Yuanfang Xiang <yf.xiang@smail.nju.edu.cn>
2025-07-30 15:35:13 +0800

    Jul 29 trained GNN on ecoli

--- scripts/data_human/label_weight_hsa.py
+++ scripts/data_human/label_weight_hsa.py
@@ -15,14 +15,15 @@ KB = RegualtoryKB(pos_trn_pth=f'dataset/human/{data_name}_KB.npz', neg_trn_pth=N
 
 Y_train = load_npz(f'dataset/human/{data_name}_Y.npz').toarray()
 X_train = load_npz(f'dataset/human/{data_name}_X.npz').toarray()
-Y_deduction = KB.deduce(torch.tensor(X_train).to('cuda'))
+Y_deduction = KB.deduce(torch.tensor(X_train).float().to('cuda'))
+Y_deduction = Y_deduction.cpu().numpy()
 
 #Y_pseudo = np.load('data_anal/abduction_results/Yp_ABL2.npy')
 #R = np.load('data_anal/abduction_results/R_ABL2.npy')
 total = len(Y_train)
 
 #gt_con_idx = (np.nonzero(np.sum((Y_deduction != Y_true) | (Y_pseudo != Y_true), axis=0) / total < .2)[0].tolist())
-kb_con_idx = (np.nonzero(np.sum((Y_deduction != 0) & (Y_deduction == Y_train), axis=0) / total > .1)[0].tolist())
+kb_con_idx = (np.nonzero(np.sum((Y_deduction != 0) & (Y_deduction == Y_train), axis=0) / total > .01)[0].tolist())
 
 #print(f'Consistent labels with Y_true\nsra: {len(labels_gt_con)}\n')
 print(f'Consistent labels with KB: {len(kb_con_idx)}\n')
@@ -47,8 +48,8 @@ df_go= pd.read_csv(f'../GEARS/{data_name}/go.csv')
 ann_data = sc.read_h5ad(f'../GEARS/{data_name}/perturb_processed.h5ad')
 df_genes= pd.DataFrame(ann_data.var)
 
-go_annot_num = np.array([len(gene2go[g]) if g in gene2go.index else 0 for g in df_genes['gene_name']], dtype=np.float32)
-go_annot_num /= np.max(go_annot_num)
+go_annot_num = np.array([len(gene2go[g]) if g in gene2go else 0 for g in df_genes['gene_name']], dtype=np.float32)
+go_annot_num = go_annot_num / np.max(go_annot_num)
 #go_annot_num = np.max(go_annot_num - .3, np.zeros_like(go_annot_num))
 print(go_annot_num)
 #y_mask_go = np.where((go_annot_num > .3) & (go_annot_num <= 1.), Y_deduction, Y_pseudo)
@@ -58,7 +59,7 @@ print(go_annot_num)
 ' weight with in-degree in GRN '
 regulatory = load_npz(f'dataset/human/{data_name}_KB.npz').toarray()
 regulatory_num = np.sum(regulatory, axis=0)
-regulatory_num /= np.max(regulatory_num)
+regulatory_num = regulatory_num / np.max(regulatory_num)
 #regulatory_num = np.max(regulatory_num - .3, np.zeros_like(regulatory_num))
 print(regulatory_num)
 #y_mask_regu = np.where((regulatory_num > .3) & (regulatory_num <= 1.), Y_deduction, Y_pseudo)
